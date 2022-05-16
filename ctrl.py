1402797feb4ecba01bd833c091f5f0d2ab794c89
fmwofmwmf <44008411+fmwofmwmf@users.noreply.github.com>
2022-05-15 22:59:22 -0600

    Update ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -1,4 +1,8 @@
 from menus import *
+
+menu = 0
+menus = {0:menu0, 1:menu1}
+
 input = {'w':False,'a':False,'s':False,'d':False, ' ':False,
           'LEFT':False, 'MIDDLE':False, 'RIGHT':False}
 cool_input = {0: {
@@ -9,15 +13,3 @@ cool_input = {0: {
                 1:{'q':lambda m=0: s_menu(m)}
               }
 
-w = 0
-
-waves = {0:[[0, 0]],
-         1:[[1, 1], [[em5, 5]]],
-         2:[[1, 1], [[em2, 5]]],
-         3:[[5, 1], [[em3, 5]]],
-         4:[[5, 1], [[em4, 5]]],
-         5:[[5, 1], [[em5, 5]]],
-         6:[[5, 1], [[em6, 5]]],
-         7:[[5, 1], [[em1, 20],[em2, 20],[em3, 20],[em4, 20],[em5, 20],[em6, 20]]],
-         8:[[5, 1], [[em1, 20]]],
-         9:[[20, 1], [[em1, 20]]],}
